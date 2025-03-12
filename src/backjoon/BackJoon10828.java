d0e2b74e07b0073c4191e705d865b36bcfc68a45
soyul <soyul9280@gmail.com>
2025-03-12 14:06:09 +0900

    스택/실버4/x

---
+++ src/backjoon/BackJoon10828.java
@@ -0,0 +1,49 @@
+package backjoon;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BackJoon10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack=new Stack<>();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            switch (st.nextToken()) {
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if(stack.empty()){
+                        bw.write(-1+"\n");
+                    }else {
+                        bw.write(stack.pop()+"\n");
+                    }
+                    break;
+                case "size":
+                    bw.write(stack.size()+"\n");
+                    break;
+                case "empty":
+                    if(stack.empty()){
+                        bw.write(1+"\n");
+                    }else {
+                        bw.write(0+"\n");
+                    }
+                    break;
+                case "top":
+                    if (stack.empty()) {
+                        bw.write(-1+"\n");
+                    }else {
+                        bw.write(stack.peek()+"\n");
+                    }
+                    break;
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
--- src/backjoon/Demo.java
+++ src/backjoon/Demo.java
@@ -1,43 +1,11 @@
 package backjoon;
 
 import java.io.*;
-import java.util.HashMap;
+import java.util.*;
 
-import java.util.StringTokenizer;
 
 public class Demo {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        HashMap<Integer, String> toStr = new HashMap<>();
-        HashMap<String, Integer> toNum = new HashMap<>();
-        for (int i = 1; i <= N; i++) {
-            String name=br.readLine();
-            toStr.put(i, name);
-            toNum.put(name, i);
-        }
-        for (int i = 0; i < M; i++) {
-            String q=br.readLine();
-            if (isNum(q)) {
-                bw.write(toStr.get(Integer.parseInt(q))+"\n");
-            }else {
-                bw.write(String.valueOf(toNum.get(q)));
-            }
-        }
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    static boolean isNum(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            if (!Character.isDigit(str.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
+    public static void main(String[] args) throws IOException, InterruptedException {
     }
 }
+
